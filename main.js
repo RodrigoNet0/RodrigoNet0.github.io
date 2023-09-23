26cf1f0bb36c690be2ad19a26f9ed6951c245d6e
Rodrigo Neto <134714036+RodrigoNet0@users.noreply.github.com>
2023-09-23 22:56:33 +0000

    botoes

---
+++ main.js
@@ -0,0 +1,33 @@
+const githubBtn = document.getElementById("githubBtn");
+const instagramBtn = document.getElementById("instagramBtn");
+const linkedinBtn = document.getElementById("linkedinBtn");
+
+function addAnimation(button) {
+    button.style.animation = "pulse 0.5s ease";
+    
+    // Limpar a animação após a conclusão (opcional)
+    button.addEventListener("animationend", () => {
+        button.style.animation = "";
+    });
+}
+
+githubBtn.addEventListener("click", () => {
+    // Redirecionar para a página do GitHub
+    window.location.href = "https://github.com/RodrigoNet0";
+    // Adicionar animação
+    addAnimation(githubBtn);
+});
+
+instagramBtn.addEventListener("click", () => {
+    // Redirecionar para a página do Instagram
+    window.location.href = "https://www.instagram.com/rodrigo.neto/";
+    // Adicionar animação
+    addAnimation(instagramBtn);
+});
+
+linkedinBtn.addEventListener("click", () => {
+    // Redirecionar para a página do LinkedIn
+    window.location.href = "https://www.linkedin.com/in/rodrigonetoo/";
+    // Adicionar animação
+    addAnimation(linkedinBtn);
+});
